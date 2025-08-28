b9097a37fcdc52fb46817efecae3ba60ca2d14b6
FaiqueCowberry <faique.cowberry@gmail.com>
2025-08-28 14:55:41 +0530

    Delete message send recieve complete | Create Group screen complete UI And Functionality

--- Components/Navigation/AppNavigator.jsx
+++ Components/Navigation/AppNavigator.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {TouchableOpacity, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DrawerScreen from './DrawerScreen';
@@ -18,92 +19,93 @@ import Attendance from '../DrawerPages/Attendance';
 import UpdateProfile from '../DrawerPages/UpdateProfile';
 import SocketChatBox from '../Chats/screens/SocketChatBox';
 import { ChatListScreen, ChatScreen } from '../Chats';
-
+import CreateGroup from '../Chats/ThreeDot/CreateGroup';
+import { useNavigation } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-      initialRouteName="Onboarding" 
-      screenOptions={{ headerShown: false }}
+      <Stack.Navigator
+        initialRouteName="Onboarding"
+        screenOptions={{ headerShown: false }}
       >
         <Stack.Screen
           name="Onboarding"
           component={Onboarding}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
+        <Stack.Screen
           name="LogIn"
           component={LogIn}
           options={{ headerShown: false }}
         />
-         <Stack.Screen
+        <Stack.Screen
           name="DrawerScreen"
           component={DrawerScreen}
           options={{ headerShown: false }}
         />
-       {/* Dummy Screen for Debugging */}
-        <Stack.Screen 
-        name="TokenDebug" 
-        component={TokenDebugScreen}
-         />
-         {/* OTP screen */}
-         <Stack.Screen 
-         name="OTPVerificationScreen"
-          component={OTPVerificationScreen} 
-          options={{ headerShown: false }} 
-          />
-          {/* Task */}
-          <Stack.Screen 
-         name="StartTask"
-          component={StartTask} 
-          options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-         name="EndTask"
-          component={EndTask} 
-          options={{ headerShown: false }} 
-          />
-          {/* DrawerPage */}
-          <Stack.Screen 
-         name="AllUsers"
-          component={AllUsers} 
-          options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-         name="Attendance"
-          component={Attendance} 
-          options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-         name="Calender"
-          component={Calender} 
-          options={{ headerShown: false }} 
-          />
-           <Stack.Screen 
-         name="TaskModal"
-          component={TaskModal} 
-          options={{ headerShown: false }} 
-          />
-           <Stack.Screen 
-         name="MyTask"
-          component={MyTask} 
-          options={{ headerShown: false }} 
-          />
-           <Stack.Screen 
-         name="UpdateStartTask"
-          component={UpdateStartTask} 
-          options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-         name="UpdateProfile"
-          component={UpdateProfile} 
-          options={{ headerShown: false }} 
-          />
-          {/* Chats Screen */}
-          {/* <Stack.Screen 
+        {/* Dummy Screen for Debugging */}
+        <Stack.Screen
+          name="TokenDebug"
+          component={TokenDebugScreen}
+        />
+        {/* OTP screen */}
+        <Stack.Screen
+          name="OTPVerificationScreen"
+          component={OTPVerificationScreen}
+          options={{ headerShown: false }}
+        />
+        {/* Task */}
+        <Stack.Screen
+          name="StartTask"
+          component={StartTask}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EndTask"
+          component={EndTask}
+          options={{ headerShown: false }}
+        />
+        {/* DrawerPage */}
+        <Stack.Screen
+          name="AllUsers"
+          component={AllUsers}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Attendance"
+          component={Attendance}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Calender"
+          component={Calender}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TaskModal"
+          component={TaskModal}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MyTask"
+          component={MyTask}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="UpdateStartTask"
+          component={UpdateStartTask}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="UpdateProfile"
+          component={UpdateProfile}
+          options={{ headerShown: false }}
+        />
+        {/* Chats Screen */}
+        {/* <Stack.Screen 
           name="ChatList" 
           component={ChatListScreen} 
           options={{ title: 'Chats' }} 
@@ -113,13 +115,16 @@ const AppNavigator = () => {
         component={ChatScreen} 
         options={{ title: 'Chat' }} 
         /> */}
-   <Stack.Screen 
-          name="SocketChatBox" 
-          component={SocketChatBox} 
-          options={{ title: 'SocketChatBox' }} 
-          />
-
-
+        <Stack.Screen
+          name="SocketChatBox"
+          component={SocketChatBox}
+          options={{ title: 'SocketChatBox' }}
+        />
+        {/* ThreeDot */}
+        <Stack.Screen
+          name="CreateGroup"
+          component={CreateGroup}
+        />
 
 
       </Stack.Navigator>
