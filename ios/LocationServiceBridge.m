401f9dcb510dfbb4ff66e306cf42e91ef2bd3b2a
FaiqueCowberry <faique.cowberry@gmail.com>
2025-09-24 14:14:38 +0530

    Tracking add in api response

--- ios/LocationServiceBridge.m
+++ ios/LocationServiceBridge.m
@@ -1,17 +1,3 @@
-// #import <React/RCTBridgeModule.h>
-
-// @interface RCT_EXTERN_MODULE(LocationServiceBridge, NSObject)
-
-// RCT_EXTERN_METHOD(startTracking)
-// RCT_EXTERN_METHOD(stopTracking)
-// RCT_EXTERN_METHOD(updateInterval:(nonnull NSNumber *)seconds)
-// RCT_EXTERN_METHOD(setAuthToken:(NSString *)token)
-// RCT_EXTERN_METHOD(setUserId:(NSString *)uid)
-// RCT_EXTERN_METHOD(setRefreshToken:(NSString *)refresh)
-// RCT_EXTERN_METHOD(syncOfflineSimple)
-
-// @end
-
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(LocationServiceBridge, NSObject)
